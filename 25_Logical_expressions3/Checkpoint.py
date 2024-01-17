fab914b6fd837e9c1f97bfca1c15a810fa136dc4
Gleb Kolonin <topsecretway@yandex.ru>
2024-01-17 13:32:04 +0300

    Проверочная

--- 25_Logical_expressions3/Checkpoint.py
+++ 25_Logical_expressions3/Checkpoint.py
@@ -18,6 +18,7 @@ for x in 0, 1:
 print('x y z w')
 for x in 0, 1:
     for y in 0, 1:
+        for z in 0, 1:
             for w in 0, 1:
                 if ( (not(y) or not(x)) and not(x == z) and w) == 1:
                     print(x, y, z, w)
