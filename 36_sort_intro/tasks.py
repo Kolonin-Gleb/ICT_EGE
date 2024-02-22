6c0fb217ca06d456ad5b9c492f22f4d640200ce0
Gleb Kolonin <topsecretway@yandex.ru>
2024-02-22 17:12:48 +0300

    Практика

--- 36_sort_intro/tasks.py
+++ 36_sort_intro/tasks.py
@@ -21,43 +21,59 @@ with open('3.txt') as file:
 '''
 
 # TODO: Переделать и перепроверить
-
 # Task 4 
 # Необходимо записать эти числа в массив и найти наибольшее количество элементов массива,
 # сумма которых будет меньше 100;
 '''
-
 with open('4.txt') as file:
     data = [int(i) for i in file]
+    N = data.pop(0)
     data = sorted(data) # по возрастанию
 
     s = 0
-    for i in range(len(data)):
+    for i in range(N):
         s += data[i]
         if s >= 100:
-            print(i - 1) # +1 или -1 ВСЁ - ТАКИ? Ведь № != index
+            print(i) # Возращаем i, т.к. индексация начинается с 0, поэтому -1 делать не нужно при нарушении условия
+            # чтобы получить кол. чисел
             break
 '''
 
 # Task 5
-# сколько файлов наибольшего объёма можно сохранить в архиве.
+# Cколько файлов наибольшего объёма можно сохранить в архиве.
 '''
-with open('0.txt') as file:
+with open('5.txt') as file:
     S, N = map(int, file.readline().split())
     print(S, N)
     
     data = [int(i) for i in file]
-    data = sorted(data, reverse=True)
+    data = sorted(data, reverse=True) # descending
     
     s = 0
     for i in range(N):
         s += data[i]
         if s >= S:
-            print(i - 1)
+            print(i) # Возращаем i, т.к. индексация начинается с 0, поэтому -1 делать не нужно при нарушении условия
+            # чтобы получить кол. чисел
             break
 '''
 
 # Task 6
+#  Определите максимальное число грузов, который может перевезти грузовик.
+# N - кол. грузов
+# M - грузоподъёмность (кг)
 """
-"""
+with open('6.txt') as file:
+    N, M = map(int, file.readline().split())
 
+    data = [int(i) for i in file]
+    data = sorted(data)
+
+    m = 0
+    for i in range(N):
+        m += data[i]
+        if m >= M:
+            print(i) # Возращаем i, т.к. индексация начинается с 0, поэтому -1 делать не нужно при нарушении условия
+            # чтобы получить кол. чисел
+            break
+"""
