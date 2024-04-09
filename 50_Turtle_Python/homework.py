ba6bf8090e9c37c35e5cf02294b095ec9c946e06
Gleb Kolonin <topsecretway@yandex.ru>
2024-04-09 11:52:51 +0300

    classwork

---
+++ 50_Turtle_Python/homework.py
@@ -0,0 +1,122 @@
+# Task 1
+'''
+from turtle import *
+# screensize(1000, 1000)
+tracer(0)
+k = 50
+left(90)
+
+
+for i in range(12):
+    forward(4*k)
+    right(90)
+
+penup()
+for x in range(-20, 20):
+    for y in range(-20, 20):
+        setpos(x*k, y*k)
+        dot(5, 'blue')
+
+done()
+'''
+
+# Task 2
+'''
+from turtle import *
+# screensize(1000, 1000)
+tracer(0)
+k = 50
+left(90)
+
+right(90)
+for i in range(7):
+    forward(6*k)
+    right(270)
+    forward(8*k)
+    right(270)
+
+penup()
+for x in range(-20, 20):
+    for y in range(-20, 20):
+        setpos(x*k, y*k)
+        dot(5, 'blue')
+
+done()
+'''
+
+# Task 3, 4, 
+'''
+from turtle import *
+screensize(1000, 4000)
+tracer(0)
+k = 50
+left(90)
+
+x = 12 # Подбор значения
+
+for i in range(10):
+    forward(x*k)
+    right(90)
+
+penup()
+for x in range(-20, 20):
+    for y in range(-20, 20):
+        setpos(x*k, y*k)
+        dot(5, 'blue')
+
+# ВАЖНО! ВКЛЮЧАЯ точки на линиях
+print(169**0.5)
+# Поэтому ответ 12
+done()
+'''
+
+# Task 6
+'''
+from turtle import *
+# screensize(1000, 1000)
+tracer(0)
+k = 50
+left(90)
+
+right(90)
+for i in range(9):
+    forward(10*k)
+    right(240)
+
+penup()
+for x in range(-20, 20):
+    for y in range(-20, 20):
+        setpos(x*k, y*k)
+        dot(5, 'blue')
+
+done()
+'''
+
+# Task 7
+'''
+from turtle import *
+screensize(1000, 3000)
+tracer(0)
+k = 50
+left(90)
+
+right(315)
+for i in range(7):
+    forward(12*k)
+    right(45)
+    forward(6*k)
+    right(135)
+
+penup()
+for x in range(-20, 20):
+    for y in range(-20, 20):
+        setpos(x*k, y*k)
+        dot(5, 'blue')
+
+done()
+'''
+
+# Task 8
+'''
+'''
+
