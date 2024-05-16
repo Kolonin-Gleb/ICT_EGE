06ee706892a3ecab5e279797d8747b4adf60beee
Spaceship24 <topsecretway@yandex.ru>
2024-05-16 20:54:03 +0300

    part of hw solved

---
+++ CooperDi/14_hw/tasks.py
@@ -0,0 +1,36 @@
+# 15314
+'''
+print("x y z w")
+for x in 0,1:
+    for y in 0,1:
+        for z in 0,1:
+            for w in 0,1:
+                if ((x and (not z)) or (y == z) or (not w)) == 0:
+                    print(x, y, z, w)
+'''
+"""
+x y z w
+0 0 1 1
+0 1 0 1
+1 0 1 1
+"""
+# wyzx
+
+# TODO: 10084 # Почему при полностью идентичной формуле и исх. таблице должен получиться другой ответ??
+'''
+print("x y z w")
+for x in 0,1:
+    for y in 0,1:
+        for z in 0,1:
+            for w in 0,1:
+                if ((x and (not z)) or (y == z) or (not w)) == 0:
+                    print(x, y, z, w)
+'''
+"""
+x y z w
+0 0 1 1
+0 1 0 1
+1 0 1 1
+"""
+# wyzx????
+
