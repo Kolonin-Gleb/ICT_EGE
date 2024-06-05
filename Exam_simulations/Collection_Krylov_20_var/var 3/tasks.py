491759e27f45693ea3d54bcb2c84375bfaa0759e
Spaceship24 <topsecretway@yandex.ru>
2024-06-05 13:46:58 +0300

    var 3 tasks 1-25 done

--- Exam_simulations/Collection_Krylov_20_var/var 3/tasks.py
+++ Exam_simulations/Collection_Krylov_20_var/var 3/tasks.py
@@ -19,9 +19,11 @@ x y z w
 1 1 1 0
 """
 
-# TODO: Task 3 - Excel
+# Task 3 - Excel
+# Answer: 1099
 
-# TODO: Task 4 - Excel (draw)
+# Task 4 - Excel (draw)
+# Answer: 12
 
 # Task 5
 '''
@@ -49,7 +51,7 @@ for N in range(1, 10_001):
 '''
 
 # Task 6
-# TODO: Как быть с сантиметрами?
+# Сантиметры - рассмтояние между точками. ОТВЕТ: 80
 '''
 from turtle import *
 # settings
@@ -80,9 +82,11 @@ for x in range(-5, 25):
         dot(5, 'blue')
 
 done()
+# Answer: 80
 '''
 
-# TODO: Task 7 - board
+# Task 7 - board
+# Answer: 1600
 
 # Task 8
 '''
@@ -102,11 +106,18 @@ for b1 in sorted("ФЛАМИНГО"):
 print(counter) # 11907
 '''
 
-# TODO: Task 9 - Excel
+# Task 9 - Excel
+# Answer: 42
 
-# TODO: Task 10 - Word
+# Task 10 - Word
+# Всего "раз" = 32
+# ---
+# Как отдельное слово "раз" = 7
+# ===
+# 32-7 = 25
 
-# TODO: Task 11 - board
+# Task 11 - board
+# Answer: 1365
 
 # Task 12
 '''
@@ -129,8 +140,16 @@ for n in range(3, 10_001):
 
 # Task 13 IP
 '''
-'''
+from ipaddress import ip_network
 
+counter = 0
+for ip in ip_network('23.140.159.160/255.255.252.0', False):
+    adr = bin(int(ip))[2:]
+    # Тут главное правильно выставить срезы для сравнения байтов числа
+    if adr[:-16].count('1') >= adr[-16:].count('1'):
+        counter +=1
+print(counter) #176
+'''
 
 # Task 14
 '''
@@ -174,13 +193,52 @@ print(F(2024) - F(2022) - 2*F(2021) - F(2020)) # 12271520
 '''
 
 # Task 17
+# НЕ ЗАБЫВАЙ ИСПОЛЬЗОВАТЬ МОДУЛЬ abs()
+# Для обработки отрицательных чисел!!! 
 '''
+def is_at_least_single_4dig(n1, n2, n3):
+    n1 = abs(n1)
+    n2 = abs(n2)
+    n3 = abs(n3)
+
+    counter_4dig = 0
+
+    if 999 < n1 and n1 < 10000:
+        counter_4dig += 1
+    if 999 < n2 and n2 < 10000:
+        counter_4dig += 1
+    if 999 < n3 and n3 < 10000:
+        counter_4dig += 1
+
+    if counter_4dig >= 1:
+        return True
+    return False
+
+f = open('17var03.txt')
+data = [int(el) for el in f]
+f.close()
+
+max_ends_90 = 0
+tripletts_counter = 0
+min_triplet_sum = 99999999999
+
+for el in data:
+    if abs(el) % 100 == 90:
+        max_ends_90 = max(max_ends_90, el)
+
+counter = 0
+for i in range(len(data) - 2):
+    if is_at_least_single_4dig(data[i], data[i+1], data[i+2]):
+        triplet_sum = (data[i] + data[i+1] + data[i+2])
+        if triplet_sum > max_ends_90:
+            counter += 1
+            min_triplet_sum = min(min_triplet_sum, triplet_sum)
+
+print(counter, min_triplet_sum) # 523 18038
 '''
-
-
-
 
 # Task 18 - Excel labirint
+# --- 223
 
 # Task 19-21 Game Theory
 '''
@@ -215,22 +273,56 @@ print("21)", [s for s in range(1, 205) if not F(s, 2) and F(s, 4)]) # minS = 45
 
 
 # Task 22 - Excel
+# Answer: 19
 '''
 '''
 
 # Task 23
 '''
+def F(x, y):
+    if x > y or x == 6 or x == 17:
+        return 0
+    if x == y:
+        return 1
+    return F(x+2, y) + F(x+3, y) + F(x*5, y)
+
+print(F(1, 31)) #  961
 '''
 
 # Task 24
+# similar to task 24 var 1
 '''
-'''
+f = open('24var03.txt')
+line = f.readline()
+f.close()
+A_indexes = []
 
+for ind, el in enumerate(line):
+    if el == "A":
+        A_indexes.append(ind)
+
+# print(A_indexes)
+
+cur_seq_len = 0
+min_seq_len = 10**10
+
+for i in range(499, len(A_indexes)):
+    cur_seq_len = A_indexes[i] - A_indexes[i-499]+1 # +1, чтобы получить ДЛИНУ!
+    min_seq_len = min(min_seq_len, cur_seq_len)
+
+print(min_seq_len) # 5678
+'''
 
 # Task 25
 '''
+from fnmatch import fnmatch
+
+for x in range(0, 10**8+1, 5171):
+    if fnmatch(str(x), '?19*8?3'):
+        print(x, x//5171)
 '''
 
-# Task 26 - by Python (queue)
+
+# TODO: Task 26 - by Excel or Python (queue)
 '''
 '''
